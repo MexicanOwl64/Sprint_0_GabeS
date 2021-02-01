4020f0d1b65094860ed827bba680fddd137a6fc5
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2021-02-01 15:12:21 -0600

    Sprint-0-Code
    This is the work that I made for Sprint 0. I Am not sure if I did this right or if there is another way to make it better.

---
+++ sprint-0-warm-up-uml-MexicanOwl64-master/Sprint 0 Warm Up/AerialVehicle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using static System.Console;
+namespace Sprint_0_Warm_Up
+{
+    public abstract class AerialVehicle
+    {
+        public int CurrentAltitude { get; set; }
+        public bool IsFlying { get; set; }
+        public int MaxAltitude { get; set; }
+
+        Engine Engine { get; set; }
+
+        bool EngineStart = false;
+        public AerialVehicle()
+        {
+            
+        }
+
+        public bool About() 
+        {
+          
+           if (IsFlying == false)
+            {
+                WriteLine("This OOPFlyingVehicle.Airplane has a max altitude of 41000ft.");
+                WriteLine("It's current altitude is " + CurrentAltitude + " ft");
+                WriteLine("OOPFlyingVehicleMidterm.Airplane engine is started");
+            }
+           else
+            {
+                WriteLine("This OOPFlyingVehicle.Airplane has a max altitude of 41000ft.");
+                WriteLine("It's current altitude is " + CurrentAltitude + " ft");
+                WriteLine("OOPFlyingVehicleMidterm.Airplane engine is not started");
+                
+            }
+            return true;
+        }
+
+        public bool TakeOff()
+        {
+            
+            if (EngineStart == false)
+            {
+                WriteLine("OOPFlyingVehicleMidterm.Airplane can't fly it's engine is not started.");
+                EngineStart = true;
+                
+                return true;
+            }
+            else
+            {
+                WriteLine("OOPFlyingVehicleMidterm.Airplane is flying");
+                return false;
+            }
+            
+        }
+
+        public void StartEngine()
+        {
+            
+        }
+
+        public void FlyDown(int howMuch)
+        {
+            if (howMuch <= 41000)
+            {
+
+                CurrentAltitude = CurrentAltitude - howMuch;
+
+            }
+
+        }
+
+        internal void FlyUp()
+        {
+         
+
+
+        }
+
+        internal void FlyUp(int HowMuch) 
+        {
+          
+            if (HowMuch <= 41000)
+            {
+
+                CurrentAltitude = CurrentAltitude + HowMuch;
+
+            }
+            else
+            {
+                
+               
+            }
+           
+        }
+    }
+}
---
+++ sprint-0-warm-up-uml-MexicanOwl64-master/Sprint 0 Warm Up/Engine.cs
@@ -0,0 +1,17 @@
+﻿namespace Sprint_0_Warm_Up
+{
+    public class Engine
+    {
+        public bool IsStarted = false;    
+   
+        public void Start()
+        {
+            IsStarted = true;
+        }
+
+        public void Stop()
+        {
+            IsStarted = false;
+        }
+    }
+}
